5ee7450f0011b4df502ee61dc9dd256e1e77b258
msmlnk <122290734+msmlnk@users.noreply.github.com>
2023-03-21 21:56:59 +0400

    Updated data

---
+++ src/index.js
@@ -0,0 +1,78 @@
+let now = new Date();
+
+let h1 = document.querySelector("h1");
+
+let date = now.getDate();
+let year = now.getFullYear();
+let minutes = String(now.getMinutes()).padStart(2, "0");
+let hours = now.getHours();
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let month = months[now.getMonth()];
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+h1.innerHTML = `${day}, ${month} ${date} </br> ${hours}:${minutes}`;
+
+function displayWeather(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function searchLocation(position) {
+  let apiKey = "5354b60afda2b7800186c06153932396";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "5354b60afda2b7800186c06153932396";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+
+let searchForm = document.querySelector("#searchform");
+searchForm.addEventListener("submit", search);
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+getCurrentLocation({ preventDefault: () => {} });
